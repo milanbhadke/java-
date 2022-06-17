aac34ce7872b795e3f15207425b7855cb67af679
hp <hp@LAPTOP-6EH4AFIK>
2022-06-17 08:28:01 +0530

    tyfty

---
+++ Month of year 5 4/src/Switchexample.java
@@ -0,0 +1,76 @@
+
+public class Switchexample {
+public static void main(String[]args) {
+String month = "june" ; 	
+	
+switch (month){
+case "january" :	
+System.out.println("this is january") ;
+break ;	
+case " february" :
+System.out.println("this is february")	 ;
+break ;	
+case "march":	
+System.out.println("this is march")	;
+break ;	
+case "april" :
+System.out.println("tjis is april")	;
+break ;	
+case "may" :	
+System.out.println("this is may") ;	
+break ;	
+case "june" :	
+System.out.println("this is june")	;
+break ;	
+case "jully" :	
+System.out.println("this is jully")	;
+break ;
+case "august" :
+System.out.println("this is august");
+break;
+case "saptember" :
+System.out.println("this is saptember");
+break ;
+case "octmber" :
+System.out.println("this is octmber");
+break ;
+case "november" :
+System.out.println("this is november") ;
+break ;
+case "december" :
+System.out.println("this is december");
+break ;
+default :
+System.out.println("this is invalid choice ");
+}
+
+
+
+
+
+
+
+
+
+
+
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
