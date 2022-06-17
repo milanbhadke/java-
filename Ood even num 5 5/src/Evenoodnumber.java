4e8697e96821c01f73689d85501c099b4973ff61
hp <hp@LAPTOP-6EH4AFIK>
2022-06-17 08:25:48 +0530

    By Milan

---
+++ Ood even num 5 5/src/Evenoodnumber.java
@@ -0,0 +1,19 @@
+
+public class Evenoodnumber {
+public static void main(String[]args) {
+	int num =10 ;
+	
+	if ((num % 2) ==0) {
+	
+	System.out.println(num +" is even num.") ;
+	}
+	
+	else {
+	System.out.println(num + "is ood num.") ;
+	
+	}
+	
+}
+	
+	
+}
