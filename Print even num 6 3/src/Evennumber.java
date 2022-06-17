33feeb4763a56332c5aa6ee41baab7a3bda6a986
hp <hp@LAPTOP-6EH4AFIK>
2022-06-17 08:31:03 +0530

    Save by Done

---
+++ Print even num 6 3/src/Evennumber.java
@@ -0,0 +1,19 @@
+
+public class Evennumber {
+
+	public static void main(String[]args) {
+	
+	for (int   i = 50 ; i <= 75 ;  i++) {
+	
+	if ( (i % 2 ) ==0 ) {
+	
+	System.out.println(i) ;
+	
+	}	
+	
+	}	
+	
+	}	
+	
+	
+}
